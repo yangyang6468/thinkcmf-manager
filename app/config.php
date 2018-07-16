b90380624d1e194936c0fd95c1ff1a4335f94729
yangyang6468 <13469984690@163.com>
2018-07-16 19:07:44 +0800

    导航开发

--- app/config.php
+++ app/config.php
@@ -1,13 +1,5 @@
 <?php
-// +----------------------------------------------------------------------
-// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
-// +----------------------------------------------------------------------
-// | Copyright (c) 2013-2018 http://www.thinkcmf.com All rights reserved.
-// +----------------------------------------------------------------------
-// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
-// +----------------------------------------------------------------------
-// | Author: Dean <zxxjjforever@163.com>
-// +----------------------------------------------------------------------
+
 if (file_exists(CMF_ROOT . "data/conf/config.php")) {
     $runtimeConfig = include CMF_ROOT . "data/conf/config.php";
 } else {
