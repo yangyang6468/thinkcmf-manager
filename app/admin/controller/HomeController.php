d707c1202dd6034ade18f4057e2a1bd6f6b8d5b2
yangyang6468 <13469984690@163.com>
2018-08-09 10:37:21 +0800

    功能完善

---
+++ app/admin/controller/HomeController.php
@@ -0,0 +1,27 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkCMF [ WE CAN DO IT MORE SIMPLE ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2013-2018 http://www.thinkcmf.com All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: 小夏 < 449134904@qq.com>
+// +----------------------------------------------------------------------
+namespace app\admin\controller;
+
+use cmf\controller\AdminBaseController;
+use think\Db;
+
+
+class HomeController extends AdminBaseController
+{
+
+    public function index()
+    {
+
+        return $this->fetch();
+    }
+
+
+}
